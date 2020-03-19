69c46f48f26fa80c7b6b90b39eb05cbf3ed7b102
MansourHussain <MansourHussain@outlook.sa>
2020-03-20 00:35:57 +0300

    modify some cases

--- main.js
+++ main.js
@@ -14,14 +14,20 @@ sub.addEventListener('click', function() {
     let flatOne = currentOne - lastOne;
     let flatTwo = currentTwo - lastTwo;
     
-    let amount = (flatOne + flatTwo)  * 0.18;
-    let vat = (amount) * 0.05;
-    let sum = (amount + vat).toFixed(2);
+    let amount1 = (flatOne)  * 0.18;
+    let amount2 = (flatTwo) * 0.18;
+    
+    let price1 = (amount1) * 0.05;
+    let price2 = (amount2 ) * 0.05;
+    let sum1 = (amount1 + price1).toFixed(2);
+    let sum2 = (amount2 + price2).toFixed(2);
+    let final_price = (sum1 + sum2);
     let result = document.querySelector('.result');
     let price = document.querySelector('.price');
-
-    result.innerHTML = `flat one: ${flatOne}<br> flat two: ${flatTwo}`;
-    price.innerHTML = `${sum} SAR`;
+    let billAmount = document.querySelector('.final_price');
+    result.innerHTML = `كمية استهلاك القاطع الأول:  ${flatOne}<br> كمية استهلاك القاطع الثاني: ${flatTwo}`;
+    price.innerHTML = `قيمة استهلاك القاطع الأول: ${sum1} ريال <br> قيمة استهلاك القاطع الثاني: ${sum2} ريال <br>`;
+    //billAmount.innerHTML = ` القيمة الإجمالية للفاتورة: ${final_price} ريال`;
     
 })
 
